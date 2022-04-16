625903ed870694906b715ddeb6d9755a454ec5c7
SalmaaHamza <salmahamza108@gmail.com>
2022-04-16 06:30:52 +0200

    init 3

--- server.js
+++ server.js
@@ -1,47 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+const io = require('socket.io')(3000)
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+const users = {}
 
-var Message = mongoose.model('Message', {
-    name: String,
-    message: String
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
-
-
-app.get('/messages', (req, res) => {
-    Message.find({}, (err, messages) => {
-        res.send(messages);
-    })
-})
-
-
-app.post('/messages', (req, res) => {
-    var message = new Message(req.body);
-    message.save((err) => {
-        if (err)
-            sendStatus(500);
-        io.emit('message', req.body);
-        res.sendStatus(200);
-    })
-
-})
-
-io.sockets.on('connection', () => {
-    console.log('a user is connected')
-})
-
-mongoose.connect("mongodb://localhost:27017/simpleChat", { useNewUrlParser: true }).then(console.log("Connecting...")).catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-});
-
-var server = http.listen(8000, () => {
-    console.log('server is running on port', server.address().port);
-});
